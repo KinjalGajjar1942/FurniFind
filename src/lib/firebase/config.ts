5e751b1422ccbf883de01c59a4185d7899f0f370
Kinjal Gajjar <kinjal.gajjar@bypeopletechnologies.com>
2025-11-12 11:16:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,11 +2,12 @@
 import { initializeApp, getApps } from 'firebase/app';
 
 const firebaseConfig = {
-  projectId: "studio-8815091537-17cb2",
-  appId: "1:349859896027:web:8f4ab95953a07c2a07cafe",
   apiKey: "AIzaSyCLKKwZowuAzPB6BkfOHzT0DfVRnPL5x9U",
   authDomain: "studio-8815091537-17cb2.firebaseapp.com",
-  messagingSenderId: "349859896027"
+  projectId: "studio-8815091537-17cb2",
+  storageBucket: "studio-8815091537-17cb2.appspot.com",
+  messagingSenderId: "349859896027",
+  appId: "1:349859896027:web:8f4ab95953a07c2a07cafe"
 };
 
 // Initialize Firebase
