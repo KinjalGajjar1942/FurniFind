b91de233f94540b6ae9f8d531700d6e0a5427438
Kinjal Gajjar <kinjal.gajjar@bypeopletechnologies.com>
2025-11-12 13:33:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8815091537-17cb2",
+  "appId": "1:349859896027:web:8f4ab95953a07c2a07cafe",
+  "apiKey": "AIzaSyCLKKwZowuAzPB6BkfOHzT0DfVRnPL5x9U",
+  "authDomain": "studio-8815091537-17cb2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "349859896027"
+};
